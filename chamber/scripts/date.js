1423948367bd1feaa58b875ae22e9c79bb9a5823
pdmoura <sudwarriormoura@gmail.com>
2025-09-13 06:58:21 -0300

    More fixes seeking for performance

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -12,19 +12,32 @@ async function getMember() {
 
         const data = await answer.json();
 
-        data.forEach(member => {
+        // Use the 'index' from forEach to track the position of each member
+        data.forEach((member, index) => {
             const div = document.createElement('div');
             div.classList.add('member');
 
+            let imgTag; // Create a variable to hold the image tag
+
+            // Check if the image is one of the first 6 (index 0-5)
+            if (index < 6) {
+                // First 6 images: Eager loading with high priority
+                imgTag = `<img src="${member.image}" alt="${member.name}" width="170" height="170" fetchpriority="high">`;
+            } else {
+                // 7th image and beyond: Lazy loading
+                imgTag = `<img src="${member.image}" alt="${member.name}" width="170" height="170" loading="lazy">`;
+            }
+
+            // Use the generated imgTag in your innerHTML
             div.innerHTML = `
-            <img src="${member.image}" alt="${member.name}" width="170" height="170" loading="lazy">
-            <h3 id="name">${member.name}</h3>
-            <p><strong>Level:</strong> ${member.membershipLevel}</p>
-            <p><strong>Phone:</strong> ${member.phone}</p>
-            <p><strong>Adress:</strong> ${member.address}</p>
-            <p><strong>Website:</strong> <a href="${member.website}" class="links">${member.website}</a></p>
-            <p> ${member.description}</p>
-            `;
+                ${imgTag}
+                <h2 class="name">${member.name}</h2>
+                <p><strong>Level:</strong> ${member.membershipLevel}</p>
+                <p><strong>Phone:</strong> ${member.phone}</p>
+                <p><strong>Address:</strong> ${member.address}</p>
+                <p><strong>Website:</strong> <a href="${member.website}" class="links" target="_blank">${member.website}</a></p>
+                <p>${member.description}</p>
+                `;
 
             container.appendChild(div);
         });
