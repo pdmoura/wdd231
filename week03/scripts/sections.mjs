a8cde97dab240e810ea80e6a0ec0f2b2054b55f6
pdmoura <sudwarriormoura@gmail.com>
2025-09-19 01:22:03 -0300

    Refactor HTML structure and styles; add modules functionality with course management -2

---
+++ week03/scripts/sections.mjs
@@ -0,0 +1,11 @@
+//set parameter "sections" for when you call the forEach function
+export function populateSections(sections) {
+  const sectionSelect = document.querySelector("#sectionNumber");
+  //remove byuiCourse from sections.forEach in orginal code since that function has been moved
+  sections.forEach((section) => {
+    const option = document.createElement("option");
+    option.value = section.sectionNumber;
+    option.textContent = `${section.sectionNumber}`;
+    sectionSelect.appendChild(option);
+  });
+}
