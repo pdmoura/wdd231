46f6b5bde20b3b2a3c1919f0a05b292457d56991
pdmoura <sudwarriormoura@gmail.com>
2025-10-03 03:02:13 -0300

    Update places data, enhance discover page, and add new styles and scripts

---
+++ chamber/scripts/discover.js
@@ -0,0 +1,68 @@
+document.addEventListener('DOMContentLoaded', function () {
+    getPlaces();
+    handleVisitorMessage();
+});
+
+
+// Fetching and displaying places from places.json
+const container = document.getElementById("places");
+
+async function getPlaces() {
+    try {
+        const answer = await fetch("data/places.json");
+
+        if (!answer.ok) {
+            throw new Error(
+                "Error while trying to get the file: " + answer.status
+            );
+        }
+
+        const data = await answer.json();
+
+        data.forEach((place) => {
+            const div = document.createElement("div");
+            div.classList.add("place");
+
+            div.innerHTML = `
+                <h2>${place.name}</h2>
+                    <figure>
+                        <img src="${place.imageUrl}" alt="Image of ${place.name}" fetchpriority="high" loading="lazy"  width="300" height="200">
+                    </figure>
+                    <address>${place.address}</address>
+                    <p>${place.description}</p>
+                    <button>Learn More</button>
+            `;
+
+            container.appendChild(div);
+        });
+    } catch (erro) {
+        console.error("Issue", erro);
+    }
+}
+
+
+// --- Visitor Message Script ---
+function handleVisitorMessage() {
+    const messageElement = document.getElementById('visitor-message');
+    const now = Date.now();
+    const lastVisit = localStorage.getItem('lastVisit');
+    let message = '';
+
+    if (!lastVisit) {
+        message = 'Welcome! Let us know if you have any questions.';
+    } else {
+        const daysDifference = Math.floor((now - parseInt(lastVisit)) / (1000 * 60 * 60 * 24));
+
+        if (daysDifference === 0) {
+            message = 'Back so soon! Awesome!';
+        } else if (daysDifference === 1) {
+            message = 'You last visited 1 day ago.';
+        } else {
+            message = `You last visited ${daysDifference} days ago.`;
+        }
+    }
+
+    messageElement.textContent = message;
+    localStorage.setItem('lastVisit', now.toString());
+
+}
